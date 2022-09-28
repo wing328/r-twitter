ce7f35ffe02a1a692252847627a656b637e4548d
William Cheng <wing328hk@gmail.com>
2022-09-27 21:16:45 -0700

    add enum validation test

--- tests/testthat/test_twitter.R
+++ tests/testthat/test_twitter.R
@@ -12,3 +12,12 @@ test_that("test Media, AnimatedGif with discriminator mapping", {
   expect_equal(a$height, 1)
   expect_equal(a$width, 2)
 })
+
+test_that("test poll with enum validation", {
+  po <- PollOption$new("this is a label", 3, 3)
+  expect_error(Poll$new("1365059861688410112", list(po), voting_status = "OPEN"), #OPEN is invalid enum value
+                 "Error! \"OPEN\" cannot be assigned to `voting_status`. Must be \"open\", \"closed\".")
+ 
+  # expect no error  
+  Poll$new("1365059861688410112", list(po), voting_status = "open")
+})
