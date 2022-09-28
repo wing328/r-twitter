b2f7535f876e9d8c76d00aaae9505e302644a30e
William Cheng <wing328hk@gmail.com>
2022-09-28 17:35:14 +0800

    Add tests for find_tweet_by_id (#18)
    * add tests for find_tweet_by_id
    
    * set bearer token

--- tests/testthat/test_twitter.R
+++ tests/testthat/test_twitter.R
@@ -21,3 +21,29 @@ test_that("test poll with enum validation", {
   # expect no error  
   Poll$new("1365059861688410112", list(po), voting_status = "open")
 })
+
+test_that("test find_tweet_by_id", {
+  var_id <- "1543815912545226752" # character | A single Tweet ID.
+  var_tweet_fields <- list("attachments") # set[character] | A comma separated list of Tweet fields to display. (Optional)
+  var_expansions <- list("attachments.media_keys") # set[character] | A comma separated list of fields to expand. (Optional)
+  var_media_fields <- list("alt_text") # set[character] | A comma separated list of Media fields to display. (Optional)
+  var_poll_fields <- list("duration_minutes") # set[character] | A comma separated list of Poll fields to display. (Optional)
+  var_user_fields <- list("created_at") # set[character] | A comma separated list of User fields to display. (Optional)
+  var_place_fields <- list("contained_within") # set[character] | A comma separated list of Place fields to display. (Optional)
+  
+  #Tweet lookup by Tweet ID
+  api_instance <- twitter_api$new()
+  # Configure HTTP bearer authorization: BearerToken
+  api_instance$api_client$bearer_token <- Sys.getenv("BEARER_TOKEN")
+  # Configure OAuth2 access token for authorization: OAuth2UserToken
+  #api_instance$api_client$access_token <- Sys.getenv("ACCESS_TOKEN")
+  result <- tryCatch(
+               # to save the result into a file, simply add the optional `data_file` parameter, e.g.
+               # api_instance$find_tweet_by_id(var_id, tweet_fields = var_tweet_fields, expansions = var_expansions, media_fields = var_media_fields, poll_fields = var_poll_fields, user_fields = var_user_fields, place_fields = var_place_fields, data_file = "result.txt"),
+               api_instance$tweets_api$find_tweet_by_id(var_id, tweet_fields = var_tweet_fields, expansions = var_expansions, media_fields = var_media_fields, poll_fields = var_poll_fields, user_fields = var_user_fields, place_fields = var_place_fields),
+               ApiException = function(ex) ex
+            )
+
+  expect_equal(result$toString(), "{\"data\":{\"id\":\"1543815912545226752\",\"text\":\"Just released #OpenAPI #Generator v6.0.1 with enhancements, bug fixes covering #java #python #PHP #kotlin #Haskell #dart #rlang and more: https://t.co/7MNHdhMJ1y. Please check it out and let us know if you've any question/feedback via https://t.co/NJmr3yWvyP #OSS #REST #API\"}}")
+
+})
